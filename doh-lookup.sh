3ff34358555ed4ba0bbdfbbcc3e93a06e61c0301
Dirk Brenken <dev@brenken.org>
2023-01-06 08:29:04 +0100

    fixes
    Signed-off-by: Dirk Brenken <dev@brenken.org>

--- doh-lookup.sh
+++ doh-lookup.sh
@@ -12,12 +12,16 @@
 #
 export LC_ALL=C
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-input="./doh-domains_overall.txt"
-upstream="1.1.1.1"
+input1="./doh-domains_overall.txt"
+input2="input2.txt"
+input3="input3.txt"
+upstream="8.8.8.8"
 check_domains="google.com heise.de openwrt.org"
 wc_tool="$(command -v wc)"
 dig_tool="$(command -v dig)"
 awk_tool="$(command -v awk)"
+: >"./${input2}"
+: >"./${input3}"
 : >"./ipv4.tmp"
 : >"./ipv6.tmp"
 : >"./domains.tmp"
@@ -25,85 +29,125 @@ awk_tool="$(command -v awk)"
 
 # sanity pre-checks
 #
-if [ ! -x "${wc_tool}" ] || [ ! -x "${dig_tool}" ] || [ ! -x "${awk_tool}" ] || [ ! -s "${input}" ] || [ -z "${upstream}" ]; then
+if [ ! -x "${wc_tool}" ] || [ ! -x "${dig_tool}" ] || [ ! -x "${awk_tool}" ] || [ ! -s "${input1}" ] || [ -z "${upstream}" ]; then
 	printf "%s\n" "ERR: general pre-check failed"
 	exit 1
 fi
 
 for domain in ${check_domains}; do
-	for resolver in ${upstream}; do
-		out="$("${dig_tool}" "@${resolver}" "${domain}" A "${domain}" AAAA +noall +answer +time=5 +tries=1 2>/dev/null)"
-		if [ -z "${out}" ]; then
-			printf "%s\n" "ERR: domain pre-check failed"
+	out="$("${dig_tool}" "${domain}" A "${domain}" AAAA +noall +answer +time=5 +tries=1 2>/dev/null)"
+	if [ -z "${out}" ]; then
+		printf "%s\n" "ERR: domain pre-check failed"
+		exit 1
+	else
+		ips="$(printf "%s" "${out}" | "${awk_tool}" '/^.*[[:space:]]+IN[[:space:]]+A{1,4}[[:space:]]+/{printf "%s ",$NF}')"
+		if [ -z "${ips}" ]; then
+			printf "%s\n" "ERR: ip pre-check failed"
 			exit 1
-		else
-			ips="$(printf "%s" "${out}" | "${awk_tool}" '/^.*[[:space:]]+IN[[:space:]]+A{1,4}[[:space:]]+/{printf "%s ",$NF}')"
-			if [ -z "${ips}" ]; then
-				printf "%s\n" "ERR: ip pre-check failed"
-				exit 1
-			fi
 		fi
-	done
+	fi
 done
 
-# domain per resolver processing
+# domain processing (first run)
 #
-cnt="1"
-domain_cnt="0"
+cnt="0"
+doh_start1="$(date "+%s")"
+doh_cnt="$("${awk_tool}" 'END{printf "%d",NR}' "./${input}" 2>/dev/null)"
+printf "%s\n" "::: Start DOH-processing, overall domains: ${doh_cnt}"
 while IFS= read -r domain; do
 	(
-		printf "%s\n" "$(date +%D-%T) ::: Start processing '${domain}' ..."
 		domain_ok="false"
-		for resolver in ${upstream}; do
-			out="$("${dig_tool}" "@${resolver}" "${domain}" A "${domain}" AAAA +noall +answer +time=10 +tries=1 2>/dev/null)"
-			if [ -n "${out}" ]; then
-				ips="$(printf "%s" "${out}" | "${awk_tool}" '/^.*[[:space:]]+IN[[:space:]]+A{1,4}[[:space:]]+/{printf "%s ",$NF}')"
-				if [ -n "${ips}" ]; then
-					for ip in ${ips}; do
-						if [ "${ip%%.*}" = "0" ] || [ -z "${ip%%::*}" ]; then
-							continue
-						else
-							if ipcalc-ng -cs "${ip}"; then
-								domain_ok="true"
-								if [ "${ip##*:}" = "${ip}" ]; then
-									printf "%-20s%s\n" "${ip}" "# ${domain}" >>"./ipv4.tmp"
-								else
-									printf "%-40s%s\n" "${ip}" "# ${domain}" >>"./ipv6.tmp"
-								fi
+		out="$("${dig_tool}" "${domain}" A "${domain}" AAAA +noall +answer +time=5 +tries=1 2>/dev/null)"
+		if [ -n "${out}" ]; then
+			ips="$(printf "%s" "${out}" | "${awk_tool}" '/^.*[[:space:]]+IN[[:space:]]+A{1,4}[[:space:]]+/{printf "%s ",$NF}')"
+			if [ -n "${ips}" ]; then
+				for ip in ${ips}; do
+					if [ "${ip%%.*}" = "0" ] || [ -z "${ip%%::*}" ]; then
+						continue
+					else
+						if ipcalc-ng -cs "${ip}"; then
+							domain_ok="true"
+							if [ "${ip##*:}" = "${ip}" ]; then
+								printf "%-20s%s\n" "${ip}" "# ${domain}" >>"./ipv4.tmp"
+							else
+								printf "%-40s%s\n" "${ip}" "# ${domain}" >>"./ipv6.tmp"
 							fi
 						fi
-					done
-				fi
+					fi
+				done
+			else
+				printf "%s\n" "$domain" >>"./${input2}"
 			fi
-		done
+		fi
 		if [ "${domain_ok}" = "false" ]; then
 			printf "%s\n" "${domain}" >>./domains_abandoned.tmp
 		else
 			printf "%s\n" "${domain}" >>./domains.tmp
 		fi
 	) &
-	domain_cnt="$((domain_cnt + 1))"
-	hold="$((cnt % 10))"
-	if [ "${hold}" = "0" ]; then
-		wait
-		cnt="1"
-	else
-		cnt="$((cnt + 1))"
-	fi
-done <"${input}"
+	hold1="$((cnt % 512))"
+	hold2="$((cnt % 4096))"
+	[ "${hold1}" = "0" ] && sleep 3
+	[ "${hold2}" = "0" ] && wait
+	cnt="$((cnt + 1))"
+done <"${input1}"
 wait
+error_cnt="$("${awk_tool}" 'END{printf "%d",NR}' "./${input2}" 2>/dev/null)"
+doh_end="$(date "+%s")"
+doh_duration="$(((doh_end - doh_start1) / 60))m $(((doh_end - doh_start1) % 60))s"
+printf "%s\n" "::: First run, duration: ${doh_duration}, processed domains: ${cnt}, error domains: ${error_cnt}"
 
-# sanity re-checks
+# domain processing (second run)
+#
+cnt="0"
+doh_start2="$(date "+%s")"
+while IFS= read -r domain; do
+	(
+		domain_ok="false"
+		out="$("${dig_tool}" "@${upstream}" "${domain}" A "${domain}" AAAA +noall +answer +time=10 +tries=1 2>/dev/null)"
+		if [ -n "${out}" ]; then
+			ips="$(printf "%s" "${out}" | "${awk_tool}" '/^.*[[:space:]]+IN[[:space:]]+A{1,4}[[:space:]]+/{printf "%s ",$NF}')"
+			if [ -n "${ips}" ]; then
+				for ip in ${ips}; do
+					if [ "${ip%%.*}" = "0" ] || [ -z "${ip%%::*}" ]; then
+						continue
+					else
+						if ipcalc-ng -cs "${ip}"; then
+							domain_ok="true"
+							if [ "${ip##*:}" = "${ip}" ]; then
+								printf "%-20s%s\n" "${ip}" "# ${domain}" >>"./ipv4.tmp"
+							else
+								printf "%-40s%s\n" "${ip}" "# ${domain}" >>"./ipv6.tmp"
+							fi
+						fi
+					fi
+				done
+			else
+				printf "%s\n" "$domain" >>"./${input3}"
+			fi
+		fi
+		if [ "${domain_ok}" = "false" ]; then
+			printf "%s\n" "${domain}" >>./domains_abandoned.tmp
+		else
+			printf "%s\n" "${domain}" >>./domains.tmp
+		fi
+	) &
+	hold1="$((cnt % 512))"
+	hold2="$((cnt % 4096))"
+	[ "${hold1}" = "0" ] && sleep 3
+	[ "${hold2}" = "0" ] && wait
+	cnt="$((cnt + 1))"
+done <"${input2}"
+wait
+error_cnt="$("${awk_tool}" 'END{printf "%d",NR}' "./${input3}" 2>/dev/null)"
+doh_end="$(date "+%s")"
+doh_duration="$(((doh_end - doh_start2) / 60))m $(((doh_end - doh_start2) % 60))s"
+printf "%s\n" "::: Second run, duration: ${doh_duration}, processed domains: ${cnt}, error domains: ${error_cnt}"
+
+# sanity re-check
 #
 if [ ! -s "./ipv4.tmp" ] || [ ! -s "./ipv6.tmp" ] || [ ! -s "./domains.tmp" ] || [ ! -f "./domains_abandoned.tmp" ]; then
 	printf "%s\n" "ERR: general re-check failed"
-	exit 1
-fi
-
-cnt_bad="$("${wc_tool}" -l "./domains_abandoned.tmp" 2>/dev/null | "${awk_tool}" '{print $1}')"
-max_bad="$(($("${wc_tool}" -l "${input}" 2>/dev/null | awk '{print $1}') * 20 / 100))"
-if [ "${cnt_bad:-"0"}" -ge "${max_bad:-"0"}" ]; then
-	printf "%s\n" "ERR: count re-check failed"
 	exit 1
 fi
 
@@ -113,7 +157,10 @@ sort -b -u -n -t. -k1,1 -k2,2 -k3,3 -k4,4 "./ipv4.tmp" >"./doh-ipv4.txt"
 sort -b -u -k1,1 "./ipv6.tmp" >"./doh-ipv6.txt"
 sort -b -u "./domains.tmp" >"./doh-domains.txt"
 sort -b -u "./domains_abandoned.tmp" >"./doh-domains_abandoned.txt"
+cnt_tmpv4="$("${awk_tool}" 'END{printf "%d",NR}' "./ipv4.tmp" 2>/dev/null)"
+cnt_tmpv6="$("${awk_tool}" 'END{printf "%d",NR}' "./ipv6.tmp" 2>/dev/null)"
 cnt_ipv4="$("${awk_tool}" 'END{printf "%d",NR}' "./doh-ipv4.txt" 2>/dev/null)"
 cnt_ipv6="$("${awk_tool}" 'END{printf "%d",NR}' "./doh-ipv6.txt" 2>/dev/null)"
-rm "./ipv4.tmp" "./ipv6.tmp" "./domains.tmp" "./domains_abandoned.tmp"
-printf "%s\n" "$(date +%D-%T) ::: Finished processing, domains: ${domain_cnt}, unique IPv4: ${cnt_ipv4:-"0"}, unique IPv6: ${cnt_ipv6:-"0"}"
+doh_end="$(date "+%s")"
+doh_duration="$(((doh_end - doh_start1) / 60))m $(((doh_end - doh_start1) % 60))s"
+printf "%s\n" "::: Finished DOH-processing, duration: ${doh_duration}, all/unique IPv4: ${cnt_tmpv4}/${cnt_ipv4}, all/unique IPv6: ${cnt_tmpv6}/${cnt_ipv6}"
