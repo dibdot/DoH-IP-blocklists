f7f08e41b5d01669b870aa367318bc8e2d63a63a
Dirk Brenken <dev@brenken.org>
2026-03-06 20:00:40 +0100

    DoH: script update 03/2026
    * added IPv4/IPv6 cache safeguards v3
    
    Signed-off-by: Dirk Brenken <dev@brenken.org>

--- doh-lookup.sh
+++ doh-lookup.sh
@@ -71,6 +71,7 @@ for domain in ${cache_domains}; do
 	done
 	wait
 	if [ ! -s "./ipv4_cache_${domain}.tmp" ]; then
+		printf "%s\n" "Iv4: no reachable IPs for ${domain}, fallback to raw cache entries"
 		"${awk_tool}" -v dom="${domain}" '$0~dom{print $1}' "./doh-ipv4.txt" | \
 		while read -r ip; do
 			[ -z "${ip}" ] && continue
@@ -95,6 +96,7 @@ for domain in ${cache_domains}; do
 	done
 	wait
 	if [ ! -s "./ipv6_cache_${domain}.tmp" ]; then
+		printf "%s\n" "IPv6: no reachable IPs for ${domain} in cache, fallback to raw cache entries"
 		"${awk_tool}" -v dom="${domain}" '$0~dom{print $1}' "./doh-ipv6.txt" | \
 		while read -r ip; do
 			[ -z "${ip}" ] && continue
